c2b107132267a187213497b238007b30df427480
jliu <liujun_0810@163.com>
2017-08-17 10:42:33 +0800

    init code

---
+++ event/src/main/java/com/guns21/event/annotation/EventBusListener.java
@@ -0,0 +1,31 @@
+package com.guns21.event.annotation;
+
+import com.guns21.event.EventBusClient;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by jliu on 2017/6/8.
+ */
+@StreamListener
+@Target( {ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+public @interface EventBusListener {
+  @AliasFor(annotation = StreamListener.class, attribute = "target")
+  String value() default EventBusClient.INPUT;
+
+  @AliasFor(annotation = StreamListener.class, attribute = "value")
+  String target() default EventBusClient.INPUT;
+
+  @AliasFor(annotation = StreamListener.class, attribute = "condition")
+  String eventType() default "";
+}
